be14f8c0f8decdd42dc67618a3c599cdab7df448
Zheng Yang <zhengyangpon69@gmail.com>
2026-03-06 01:34:48 +0800

    changed to postgres settings...

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'elearning.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR/'db.sqlite3'}"),
+        conn_max_age=600,
+    )
 }
 
 DATABASES = {
