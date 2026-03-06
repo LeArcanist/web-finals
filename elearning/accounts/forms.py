272ffc8100036d3e33cd90c9a89b380f96f41046
Zheng Yang <zhengyangpon69@gmail.com>
2026-03-06 18:00:24 +0800

    beautified register form css

--- elearning/accounts/forms.py
+++ elearning/accounts/forms.py
@@ -6,6 +6,48 @@ from .models import User
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "email", "real_name", "role", "photo", "password1", "password2")
+        fields = (
+            "username",
+            "email",
+            "real_name",
+            "role",
+            "photo",
+            "password1",
+            "password2",
+        )
 
     email = forms.EmailField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter username",
+        })
+        self.fields["email"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter email",
+        })
+        self.fields["real_name"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter your real name",
+        })
+        self.fields["role"].widget.attrs.update({
+            "class": "form-control",
+        })
+        self.fields["photo"].widget.attrs.update({
+            "class": "form-control",
+        })
+        self.fields["password1"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Enter password",
+        })
+        self.fields["password2"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Confirm password",
+        })
+
+        self.fields["password1"].help_text = ""
+        self.fields["password2"].help_text = ""
+        self.fields["username"].help_text = ""
